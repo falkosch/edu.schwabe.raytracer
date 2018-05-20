8b28f669cbbe3414cdd12712d54792d29cd8ff42
Falko Schwabe <falkosch@hotmail.de>
2018-05-20 23:09:56 +0200

    use noexcept where suggested by code analysis in selects

--- trunk/vectorization/include/vectorization/selects.h
+++ trunk/vectorization/include/vectorization/selects.h
@@ -17,7 +17,7 @@ namespace vectorization
     //{ boolMask
 
     template <typename T>
-    inline const T boolMask(const bool mask)
+    inline const T boolMask(const bool mask) noexcept
     {
         static_assert(std::is_integral<T>::value, "boolMask<T> must be an integral type");
         return static_cast<T>(-static_cast<typename std::make_signed<T>::type>(mask));
@@ -29,7 +29,7 @@ namespace vectorization
 #pragma region blendMasked()
     //{ blendMasked()
 
-#define BLEND_MASKED(type) const type blendMasked(const type onBitNotSet, const type onBitSet, const type mask)
+#define BLEND_MASKED(type) const type blendMasked(const type onBitNotSet, const type onBitSet, const type mask) noexcept
     BLEND_MASKED(bool);
     BLEND_MASKED(long);
     BLEND_MASKED(unsigned long);
@@ -43,15 +43,15 @@ namespace vectorization
     BLEND_MASKED(UInt_64);
 #undef BLEND_MASKED
 
-    const Float_32 blendMasked(const Float_32 onBitNotSet, const Float_32 onBitSet, const BoolTypes<Float_32>::Type mask);
+    const Float_32 blendMasked(const Float_32 onBitNotSet, const Float_32 onBitSet, const BoolTypes<Float_32>::Type mask) noexcept;
 
-    const Float_64 blendMasked(const Float_64 onBitNotSet, const Float_64 onBitSet, const BoolTypes<Float_64>::Type mask);
+    const Float_64 blendMasked(const Float_64 onBitNotSet, const Float_64 onBitSet, const BoolTypes<Float_64>::Type mask) noexcept;
 
-    const PackedInts_128 blendMasked(const PackedInts_128 & onBitNotSet, const PackedInts_128 & onBitSet, const PackedInts_128 & mask);
+    const PackedInts_128 blendMasked(const PackedInts_128 & onBitNotSet, const PackedInts_128 & onBitSet, const PackedInts_128 & mask) noexcept;
 
-    const PackedFloat4_128 blendMasked(const PackedFloat4_128 & onBitNotSet, const PackedFloat4_128 & onBitSet, const PackedFloat4_128 & mask);
+    const PackedFloat4_128 blendMasked(const PackedFloat4_128 & onBitNotSet, const PackedFloat4_128 & onBitSet, const PackedFloat4_128 & mask) noexcept;
 
-    const PackedFloat2_128 blendMasked(const PackedFloat2_128 & onBitNotSet, const PackedFloat2_128 & onBitSet, const PackedFloat2_128 & mask);
+    const PackedFloat2_128 blendMasked(const PackedFloat2_128 & onBitNotSet, const PackedFloat2_128 & onBitSet, const PackedFloat2_128 & mask) noexcept;
 
     //}
 #pragma endregion
@@ -59,7 +59,7 @@ namespace vectorization
 #pragma region select()
     //{ select()
 
-#define SELECT(type) const type select(const type mask, const type onTrue, const type onFalse)
+#define SELECT(type) const type select(const type mask, const type onTrue, const type onFalse) noexcept
     SELECT(bool);
     SELECT(long);
     SELECT(unsigned long);
@@ -73,7 +73,7 @@ namespace vectorization
     SELECT(UInt_64);
 #undef SELECT
 
-#define BOOLEAN_SELECT(type) const type select(const bool mask, const type onTrue, const type onFalse)
+#define BOOLEAN_SELECT(type) const type select(const bool mask, const type onTrue, const type onFalse) noexcept
     BOOLEAN_SELECT(long);
     BOOLEAN_SELECT(unsigned long);
     BOOLEAN_SELECT(Int_8);
@@ -86,26 +86,26 @@ namespace vectorization
     BOOLEAN_SELECT(UInt_64);
 #undef BOOLEAN_SELECT
 
-    const Float_32 select(const bool mask, const Float_32 onTrue, const Float_32 onFalse);
+    const Float_32 select(const bool mask, const Float_32 onTrue, const Float_32 onFalse) noexcept;
 
-    const Float_32 select(const BoolTypes<Float_32>::Type mask, const Float_32 onTrue, const Float_32 onFalse);
+    const Float_32 select(const BoolTypes<Float_32>::Type mask, const Float_32 onTrue, const Float_32 onFalse) noexcept;
 
-    const Float_64 select(const bool mask, const Float_64 onTrue, const Float_64 onFalse);
+    const Float_64 select(const bool mask, const Float_64 onTrue, const Float_64 onFalse) noexcept;
 
-    const Float_64 select(const BoolTypes<Float_64>::Type mask, const Float_64 onTrue, const Float_64 onFalse);
+    const Float_64 select(const BoolTypes<Float_64>::Type mask, const Float_64 onTrue, const Float_64 onFalse) noexcept;
 
-    const PackedInts_128 select(const bool mask, const PackedInts_128 & onTrue, const PackedInts_128 & onFalse);
+    const PackedInts_128 select(const bool mask, const PackedInts_128 & onTrue, const PackedInts_128 & onFalse) noexcept;
 
-    const PackedFloat4_128 select(const bool mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse);
+    const PackedFloat4_128 select(const bool mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse) noexcept;
 
-    const PackedFloat4_128 select(const PackedFloat4_128 & mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse);
+    const PackedFloat4_128 select(const PackedFloat4_128 & mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse) noexcept;
 
-    const PackedFloat2_128 select(const bool mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse);
+    const PackedFloat2_128 select(const bool mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse) noexcept;
 
-    const PackedFloat2_128 select(const PackedFloat2_128 & mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse);
+    const PackedFloat2_128 select(const PackedFloat2_128 & mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse) noexcept;
 
     template <typename T>
-    inline const T & select(const bool mask, const T & onTrue, const T & onFalse)
+    inline const T & select(const bool mask, const T & onTrue, const T & onFalse) noexcept
     {
         return *(mask ? &onTrue : &onFalse);
     }
@@ -116,7 +116,7 @@ namespace vectorization
     template <typename T>
     struct BoolSelector < T, false >
     {
-        inline const T & operator()(const T & onTrue, const T & onFalse)
+        inline const T & operator()(const T & onTrue, const T & onFalse) noexcept
         {
             return onFalse;
         }
@@ -125,7 +125,7 @@ namespace vectorization
     template <typename T>
     struct BoolSelector < T, true >
     {
-        inline const T & operator()(const T & onTrue, const T & onFalse)
+        inline const T & operator()(const T & onTrue, const T & onFalse) noexcept
         {
             return onTrue;
         }
--- trunk/vectorization/src/vectorization/selects.cpp
+++ trunk/vectorization/src/vectorization/selects.cpp
@@ -4,271 +4,277 @@ namespace vectorization
 {
 
 #pragma region blendMasked()
-    //{ blendMasked()
+	//{ blendMasked()
 
-    const bool blendMasked(const bool onBitNotSet, const bool onBitSet, const bool mask)
-    {
-        return mask ? onBitSet : onBitNotSet;
-    }
+	const bool blendMasked(const bool onBitNotSet, const bool onBitSet, const bool mask) noexcept
+	{
+		return mask ? onBitSet : onBitNotSet;
+	}
 
-    const long blendMasked(const long onBitNotSet, const long onBitSet, const long mask)
-    {
-        return (mask & onBitSet) | (~mask & onBitNotSet);
-    }
+	const long blendMasked(const long onBitNotSet, const long onBitSet, const long mask) noexcept
+	{
+		return (mask & onBitSet) | (~mask & onBitNotSet);
+	}
 
-    const unsigned long blendMasked(const unsigned long onBitNotSet, const unsigned long onBitSet, const unsigned long mask)
-    {
-        return (mask & onBitSet) | (~mask & onBitNotSet);
-    }
+	const unsigned long blendMasked(const unsigned long onBitNotSet, const unsigned long onBitSet, const unsigned long mask) noexcept
+	{
+		return (mask & onBitSet) | (~mask & onBitNotSet);
+	}
 
-    const Int_8 blendMasked(const Int_8 onBitNotSet, const Int_8 onBitSet, const Int_8 mask)
-    {
-        return static_cast<Int_8>((mask & onBitSet) | (~mask & onBitNotSet));
-    }
+	const Int_8 blendMasked(const Int_8 onBitNotSet, const Int_8 onBitSet, const Int_8 mask) noexcept
+	{
+		return static_cast<Int_8>((mask & onBitSet) | (~mask & onBitNotSet));
+	}
 
-    const UInt_8 blendMasked(const UInt_8 onBitNotSet, const UInt_8 onBitSet, const UInt_8 mask)
-    {
-        return static_cast<UInt_8>((mask & onBitSet) | (~mask & onBitNotSet));
-    }
+	const UInt_8 blendMasked(const UInt_8 onBitNotSet, const UInt_8 onBitSet, const UInt_8 mask) noexcept
+	{
+		return static_cast<UInt_8>((mask & onBitSet) | (~mask & onBitNotSet));
+	}
 
-    const Int_16 blendMasked(const Int_16 onBitNotSet, const Int_16 onBitSet, const Int_16 mask)
-    {
-        return static_cast<Int_16>((mask & onBitSet) | (~mask & onBitNotSet));
-    }
+	const Int_16 blendMasked(const Int_16 onBitNotSet, const Int_16 onBitSet, const Int_16 mask) noexcept
+	{
+		return static_cast<Int_16>((mask & onBitSet) | (~mask & onBitNotSet));
+	}
 
-    const UInt_16 blendMasked(const UInt_16 onBitNotSet, const UInt_16 onBitSet, const UInt_16 mask)
-    {
-        return static_cast<UInt_16>((mask & onBitSet) | (~mask & onBitNotSet));
-    }
+	const UInt_16 blendMasked(const UInt_16 onBitNotSet, const UInt_16 onBitSet, const UInt_16 mask) noexcept
+	{
+		return static_cast<UInt_16>((mask & onBitSet) | (~mask & onBitNotSet));
+	}
 
-    const Int_32 blendMasked(const Int_32 onBitNotSet, const Int_32 onBitSet, const Int_32 mask)
-    {
-        return (mask & onBitSet) | (~mask & onBitNotSet);
-    }
+	const Int_32 blendMasked(const Int_32 onBitNotSet, const Int_32 onBitSet, const Int_32 mask) noexcept
+	{
+		return (mask & onBitSet) | (~mask & onBitNotSet);
+	}
 
-    const UInt_32 blendMasked(const UInt_32 onBitNotSet, const UInt_32 onBitSet, const UInt_32 mask)
-    {
-        return (mask & onBitSet) | (~mask & onBitNotSet);
-    }
+	const UInt_32 blendMasked(const UInt_32 onBitNotSet, const UInt_32 onBitSet, const UInt_32 mask) noexcept
+	{
+		return (mask & onBitSet) | (~mask & onBitNotSet);
+	}
 
-    const Int_64 blendMasked(const Int_64 onBitNotSet, const Int_64 onBitSet, const Int_64 mask)
-    {
-        return (mask & onBitSet) | (~mask & onBitNotSet);
-    }
+	const Int_64 blendMasked(const Int_64 onBitNotSet, const Int_64 onBitSet, const Int_64 mask) noexcept
+	{
+		return (mask & onBitSet) | (~mask & onBitNotSet);
+	}
 
-    const UInt_64 blendMasked(const UInt_64 onBitNotSet, const UInt_64 onBitSet, const UInt_64 mask)
-    {
-        return (mask & onBitSet) | (~mask & onBitNotSet);
-    }
+	const UInt_64 blendMasked(const UInt_64 onBitNotSet, const UInt_64 onBitSet, const UInt_64 mask) noexcept
+	{
+		return (mask & onBitSet) | (~mask & onBitNotSet);
+	}
 
-    const Float_32 blendMasked(const Float_32 onBitNotSet, const Float_32 onBitSet, const BoolTypes<Float_32>::Type mask)
-    {
-        return _mm_cvtss_f32(blendMasked(
-            _mm_set_ss(onBitNotSet),
-            _mm_set_ss(onBitSet),
-            _mm_castsi128_ps(_mm_set1_epi32(static_cast<int>(mask)))));
-    }
+	const Float_32 blendMasked(const Float_32 onBitNotSet, const Float_32 onBitSet, const BoolTypes<Float_32>::Type mask) noexcept
+	{
+		return _mm_cvtss_f32(
+			blendMasked(
+				_mm_set_ss(onBitNotSet),
+				_mm_set_ss(onBitSet),
+				_mm_castsi128_ps(_mm_set1_epi32(static_cast<int>(mask)))
+			)
+		);
+	}
 
-    const Float_64 blendMasked(const Float_64 onBitNotSet, const Float_64 onBitSet, const BoolTypes<Float_64>::Type mask)
-    {
+	const Float_64 blendMasked(const Float_64 onBitNotSet, const Float_64 onBitSet, const BoolTypes<Float_64>::Type mask) noexcept
+	{
 #ifdef ARCH_X64
-        const PackedInts_128 m = _mm_set1_epi64x(mask);
+		const PackedInts_128 m = _mm_set1_epi64x(mask);
 #else
-        PackedInts_128 m;
-        reinterpret_cast<BoolTypes<Float_64>::Type * const>(&m)[VectorIndices::X] = mask;
+		PackedInts_128 m;
+		reinterpret_cast<BoolTypes<Float_64>::Type * const>(&m)[VectorIndices::X] = mask;
 #endif
-        return _mm_cvtsd_f64(blendMasked(
-            _mm_set_sd(onBitNotSet),
-            _mm_set_sd(onBitSet),
-            _mm_castsi128_pd(m)));
-    }
+		return _mm_cvtsd_f64(
+			blendMasked(
+				_mm_set_sd(onBitNotSet),
+				_mm_set_sd(onBitSet),
+				_mm_castsi128_pd(m)
+			)
+		);
+	}
 
-    const PackedInts_128 blendMasked(const PackedInts_128 & onBitNotSet, const PackedInts_128 & onBitSet, const PackedInts_128 & mask)
-    {
+	const PackedInts_128 blendMasked(const PackedInts_128 & onBitNotSet, const PackedInts_128 & onBitSet, const PackedInts_128 & mask) noexcept
+	{
 #if VECTORIZATION_INTRINSICS_LEVEL < VECTORIZATION_SSE4
-        return _mm_or_si128(_mm_and_si128(mask, onBitSet), _mm_andnot_si128(mask, onBitNotSet));
+		return _mm_or_si128(_mm_and_si128(mask, onBitSet), _mm_andnot_si128(mask, onBitNotSet));
 #else
-        return _mm_blendv_epi8(onBitNotSet, onBitSet, mask);
+		return _mm_blendv_epi8(onBitNotSet, onBitSet, mask);
 #endif
-    }
+	}
 
-    const PackedFloat4_128 blendMasked(const PackedFloat4_128 & onBitNotSet, const PackedFloat4_128 & onBitSet, const PackedFloat4_128 & mask)
-    {
+	const PackedFloat4_128 blendMasked(const PackedFloat4_128 & onBitNotSet, const PackedFloat4_128 & onBitSet, const PackedFloat4_128 & mask) noexcept
+	{
 #if VECTORIZATION_INTRINSICS_LEVEL < VECTORIZATION_SSE4
-        return _mm_or_ps(_mm_and_ps(mask, onBitSet), _mm_andnot_ps(mask, onBitNotSet));
+		return _mm_or_ps(_mm_and_ps(mask, onBitSet), _mm_andnot_ps(mask, onBitNotSet));
 #else
-        return _mm_blendv_ps(onBitNotSet, onBitSet, mask);
+		return _mm_blendv_ps(onBitNotSet, onBitSet, mask);
 #endif
-    }
+	}
 
-    const PackedFloat2_128 blendMasked(const PackedFloat2_128 & onBitNotSet, const PackedFloat2_128 & onBitSet, const PackedFloat2_128 & mask)
-    {
+	const PackedFloat2_128 blendMasked(const PackedFloat2_128 & onBitNotSet, const PackedFloat2_128 & onBitSet, const PackedFloat2_128 & mask) noexcept
+	{
 #if VECTORIZATION_INTRINSICS_LEVEL < VECTORIZATION_SSE4
-        return _mm_or_pd(_mm_and_pd(mask, onBitSet), _mm_andnot_pd(mask, onBitNotSet));
+		return _mm_or_pd(_mm_and_pd(mask, onBitSet), _mm_andnot_pd(mask, onBitNotSet));
 #else
-        return _mm_blendv_pd(onBitNotSet, onBitSet, mask);
+		return _mm_blendv_pd(onBitNotSet, onBitSet, mask);
 #endif
-    }
+	}
 
-    //}
+	//}
 #pragma endregion
 
 #pragma region select()
-    //{ select()
+	//{ select()
 
-    const bool select(const bool mask, const bool onTrue, const bool onFalse)
-    {
-        return mask ? onTrue : onFalse;
-    }
+	const bool select(const bool mask, const bool onTrue, const bool onFalse) noexcept
+	{
+		return mask ? onTrue : onFalse;
+	}
 
-    const long select(const long mask, const long onTrue, const long onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<long>() : Zero<long>());
-    }
+	const long select(const long mask, const long onTrue, const long onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<long>() : Zero<long>());
+	}
 
-    const unsigned long select(const unsigned long mask, const unsigned long onTrue, const unsigned long onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<unsigned long>() : Zero<unsigned long>());
-    }
+	const unsigned long select(const unsigned long mask, const unsigned long onTrue, const unsigned long onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<unsigned long>() : Zero<unsigned long>());
+	}
 
-    const Int_8 select(const Int_8 mask, const Int_8 onTrue, const Int_8 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_8>() : Zero<Int_8>());
-    }
+	const Int_8 select(const Int_8 mask, const Int_8 onTrue, const Int_8 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_8>() : Zero<Int_8>());
+	}
 
-    const UInt_8 select(const UInt_8 mask, const UInt_8 onTrue, const UInt_8 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_8>() : Zero<UInt_8>());
-    }
+	const UInt_8 select(const UInt_8 mask, const UInt_8 onTrue, const UInt_8 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_8>() : Zero<UInt_8>());
+	}
 
-    const Int_16 select(const Int_16 mask, const Int_16 onTrue, const Int_16 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_16>() : Zero<Int_16>());
-    }
+	const Int_16 select(const Int_16 mask, const Int_16 onTrue, const Int_16 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_16>() : Zero<Int_16>());
+	}
 
-    const UInt_16 select(const UInt_16 mask, const UInt_16 onTrue, const UInt_16 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_16>() : Zero<UInt_16>());
-    }
+	const UInt_16 select(const UInt_16 mask, const UInt_16 onTrue, const UInt_16 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_16>() : Zero<UInt_16>());
+	}
 
-    const Int_32 select(const Int_32 mask, const Int_32 onTrue, const Int_32 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_32>() : Zero<Int_32>());
-    }
+	const Int_32 select(const Int_32 mask, const Int_32 onTrue, const Int_32 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_32>() : Zero<Int_32>());
+	}
 
-    const UInt_32 select(const UInt_32 mask, const UInt_32 onTrue, const UInt_32 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_32>() : Zero<UInt_32>());
-    }
+	const UInt_32 select(const UInt_32 mask, const UInt_32 onTrue, const UInt_32 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_32>() : Zero<UInt_32>());
+	}
 
-    const Int_64 select(const Int_64 mask, const Int_64 onTrue, const Int_64 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_64>() : Zero<Int_64>());
-    }
+	const Int_64 select(const Int_64 mask, const Int_64 onTrue, const Int_64 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<Int_64>() : Zero<Int_64>());
+	}
 
-    const UInt_64 select(const UInt_64 mask, const UInt_64 onTrue, const UInt_64 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_64>() : Zero<UInt_64>());
-    }
+	const UInt_64 select(const UInt_64 mask, const UInt_64 onTrue, const UInt_64 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<UInt_64>() : Zero<UInt_64>());
+	}
 
-    const long select(const bool mask, const long onTrue, const long onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<long>(mask));
-    }
+	const long select(const bool mask, const long onTrue, const long onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<long>(mask));
+	}
 
-    const unsigned long select(const bool mask, const unsigned long onTrue, const unsigned long onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<unsigned long>(mask));
-    }
+	const unsigned long select(const bool mask, const unsigned long onTrue, const unsigned long onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<unsigned long>(mask));
+	}
 
-    const Int_8 select(const bool mask, const Int_8 onTrue, const Int_8 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<Int_8>(mask));
-    }
+	const Int_8 select(const bool mask, const Int_8 onTrue, const Int_8 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<Int_8>(mask));
+	}
 
-    const UInt_8 select(const bool mask, const UInt_8 onTrue, const UInt_8 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<UInt_8>(mask));
-    }
+	const UInt_8 select(const bool mask, const UInt_8 onTrue, const UInt_8 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<UInt_8>(mask));
+	}
 
-    const Int_16 select(const bool mask, const Int_16 onTrue, const Int_16 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<Int_16>(mask));
-    }
+	const Int_16 select(const bool mask, const Int_16 onTrue, const Int_16 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<Int_16>(mask));
+	}
 
-    const UInt_16 select(const bool mask, const UInt_16 onTrue, const UInt_16 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<UInt_16>(mask));
-    }
+	const UInt_16 select(const bool mask, const UInt_16 onTrue, const UInt_16 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<UInt_16>(mask));
+	}
 
-    const Int_32 select(const bool mask, const Int_32 onTrue, const Int_32 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<Int_32>(mask));
-    }
+	const Int_32 select(const bool mask, const Int_32 onTrue, const Int_32 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<Int_32>(mask));
+	}
 
-    const UInt_32 select(const bool mask, const UInt_32 onTrue, const UInt_32 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<UInt_32>(mask));
-    }
+	const UInt_32 select(const bool mask, const UInt_32 onTrue, const UInt_32 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<UInt_32>(mask));
+	}
 
-    const Int_64 select(const bool mask, const Int_64 onTrue, const Int_64 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<Int_64>(mask));
-    }
+	const Int_64 select(const bool mask, const Int_64 onTrue, const Int_64 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<Int_64>(mask));
+	}
 
-    const UInt_64 select(const bool mask, const UInt_64 onTrue, const UInt_64 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<UInt_64>(mask));
-    }
+	const UInt_64 select(const bool mask, const UInt_64 onTrue, const UInt_64 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<UInt_64>(mask));
+	}
 
-    const Float_32 select(const bool mask, const Float_32 onTrue, const Float_32 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<BoolTypes<Float_32>::Type>(mask));
-    }
+	const Float_32 select(const bool mask, const Float_32 onTrue, const Float_32 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<BoolTypes<Float_32>::Type>(mask));
+	}
 
-    const Float_32 select(const BoolTypes<Float_32>::Type mask, const Float_32 onTrue, const Float_32 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<BoolTypes<Float_32>::Type>() : Zero<BoolTypes<Float_32>::Type>());
-    }
+	const Float_32 select(const BoolTypes<Float_32>::Type mask, const Float_32 onTrue, const Float_32 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<BoolTypes<Float_32>::Type>() : Zero<BoolTypes<Float_32>::Type>());
+	}
 
-    const Float_64 select(const bool mask, const Float_64 onTrue, const Float_64 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, boolMask<BoolTypes<Float_64>::Type>(mask));
-    }
+	const Float_64 select(const bool mask, const Float_64 onTrue, const Float_64 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, boolMask<BoolTypes<Float_64>::Type>(mask));
+	}
 
-    const Float_64 select(const BoolTypes<Float_64>::Type mask, const Float_64 onTrue, const Float_64 onFalse)
-    {
-        return blendMasked(onFalse, onTrue, mask ? MaskAll<BoolTypes<Float_64>::Type>() : Zero<BoolTypes<Float_64>::Type>());
-    }
+	const Float_64 select(const BoolTypes<Float_64>::Type mask, const Float_64 onTrue, const Float_64 onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, mask ? MaskAll<BoolTypes<Float_64>::Type>() : Zero<BoolTypes<Float_64>::Type>());
+	}
 
-    const PackedInts_128 select(const bool mask, const PackedInts_128 & onTrue, const PackedInts_128 & onFalse)
-    {
-        return blendMasked(onFalse, onTrue, _mm_set1_epi32(boolMask<int>(mask)));
-    }
+	const PackedInts_128 select(const bool mask, const PackedInts_128 & onTrue, const PackedInts_128 & onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, _mm_set1_epi32(boolMask<int>(mask)));
+	}
 
-    const PackedFloat4_128 select(const bool mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse)
-    {
-        return blendMasked(onFalse, onTrue, _mm_castsi128_ps(_mm_set1_epi32(boolMask<int>(mask))));
-    }
+	const PackedFloat4_128 select(const bool mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, _mm_castsi128_ps(_mm_set1_epi32(boolMask<int>(mask))));
+	}
 
-    const PackedFloat4_128 select(const PackedFloat4_128 & mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse)
-    {
-        // reverse onTrue & onFalse since we test for mask components to be zero
-        return blendMasked(onTrue, onFalse, _mm_cmpeq_ps(mask, Zero<PackedFloat4_128>()));
-    }
+	const PackedFloat4_128 select(const PackedFloat4_128 & mask, const PackedFloat4_128 & onTrue, const PackedFloat4_128 & onFalse) noexcept
+	{
+		// reverse onTrue & onFalse since we test for mask components to be zero
+		return blendMasked(onTrue, onFalse, _mm_cmpeq_ps(mask, Zero<PackedFloat4_128>()));
+	}
 
-    const PackedFloat2_128 select(const bool mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse)
-    {
-        return blendMasked(onFalse, onTrue, _mm_castsi128_pd(_mm_set1_epi32(boolMask<int>(mask))));
-    }
+	const PackedFloat2_128 select(const bool mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse) noexcept
+	{
+		return blendMasked(onFalse, onTrue, _mm_castsi128_pd(_mm_set1_epi32(boolMask<int>(mask))));
+	}
 
-    const PackedFloat2_128 select(const PackedFloat2_128 & mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse)
-    {
-        // reverse onTrue & onFalse since we test for mask components to be zero
-        return blendMasked(onTrue, onFalse, _mm_cmpeq_pd(mask, Zero<PackedFloat2_128>()));
-    }
+	const PackedFloat2_128 select(const PackedFloat2_128 & mask, const PackedFloat2_128 & onTrue, const PackedFloat2_128 & onFalse) noexcept
+	{
+		// reverse onTrue & onFalse since we test for mask components to be zero
+		return blendMasked(onTrue, onFalse, _mm_cmpeq_pd(mask, Zero<PackedFloat2_128>()));
+	}
 
-    //}
+	//}
 #pragma endregion
 
 }
