da4a23c5844aaccbf9ed262121615504ac5734a3
Falko Schwabe <falkosch@hotmail.de>
2018-05-20 22:58:13 +0200

    change macro for the signature of the class-specific allocators for aligned memory allocation to be consistent with the stl specs

--- trunk/vectorization/include/vectorization/intrinsics.h
+++ trunk/vectorization/include/vectorization/intrinsics.h
@@ -10,6 +10,7 @@
 #include "datatypes.h"
 
 #include <cassert>
+#include <new>
 
 #if defined(__GNUC__)
 
@@ -46,252 +47,290 @@
 #define ALIGNED(alignment, decleration) BEGIN_ALIGNED(BEST_ALIGNMENT) decleration END_ALIGNED(BEST_ALIGNMENT)
 
 #define ALIGNED_ALLOCATORS(alignment) \
-    static void * operator new(const vectorization::ASizeT size) { return vectorization::alloc(size, alignment); } \
-    static void operator delete(void * alignedData) { vectorization::free(alignedData); } \
-    static void * operator new[](const vectorization::ASizeT size) { return vectorization::alloc(size, alignment); } \
-    static void operator delete[](void * alignedData) { vectorization::free(alignedData); }
+    static void * operator new(std::size_t size) { return vectorization::alloc(size, alignment); } \
+	static void * operator new(std::size_t size, const std::nothrow_t &) noexcept { return operator new(size); } \
+	static void * operator new(std::size_t size, std::align_val_t requestedAlignment) { return vectorization::alloc(size, requestedAlignment); } \
+	static void * operator new(std::size_t size, std::align_val_t requestedAlignment, const std::nothrow_t &) noexcept { return operator new(size, requestedAlignment); } \
+	\
+	static void operator delete(void * ptr) noexcept { vectorization::free(ptr); } \
+	static void operator delete(void * ptr, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	static void operator delete(void * ptr, std::align_val_t) noexcept { operator delete(ptr); } \
+	static void operator delete(void * ptr, std::align_val_t, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	static void operator delete(void * ptr, std::size_t) noexcept { operator delete(ptr); } \
+	static void operator delete(void * ptr, std::size_t, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	static void operator delete(void * ptr, std::size_t, std::align_val_t) noexcept { operator delete(ptr); } \
+	static void operator delete(void * ptr, std::size_t, std::align_val_t, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	\
+    static void * operator new[](std::size_t size) { return operator new(size); } \
+	static void * operator new[](std::size_t size, const std::nothrow_t &) noexcept { return operator new(size); } \
+	static void * operator new[](std::size_t size, std::align_val_t requestedAlignment) { return operator new(size, requestedAlignment); } \
+	static void * operator new[](std::size_t size, std::align_val_t requestedAlignment, const std::nothrow_t & ) noexcept { return operator new(size, requestedAlignment); } \
+	\
+	static void operator delete[](void * ptr) noexcept { vectorization::free(ptr); } \
+	static void operator delete[](void * ptr, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	static void operator delete[](void * ptr, std::align_val_t) noexcept { operator delete(ptr); } \
+	static void operator delete[](void * ptr, std::align_val_t, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	static void operator delete[](void * ptr, std::size_t) noexcept { operator delete(ptr); } \
+	static void operator delete[](void * ptr, std::size_t, const std::nothrow_t &) noexcept { operator delete(ptr); } \
+	static void operator delete[](void * ptr, std::size_t, std::align_val_t) noexcept { operator delete(ptr); } \
+	static void operator delete[](void * ptr, std::size_t, std::align_val_t, const std::nothrow_t &) noexcept { operator delete(ptr); }
 
 
-#define BLEND_MASK(X, Y, Z, W) ((X & 1) << 0) | ((Y & 1) << 1) | ((Z & 1) << 2) | ((W & 1) << 3)
+#define BLEND_MASK(X, Y, Z, W) ( \
+		((X & 1) << 0) \
+		| ((Y & 1) << 1) \
+		| ((Z & 1) << 2) \
+		| ((W & 1) << 3) \
+		)
 
-#define BLEND_MASK_i4(X, Y, Z, W) \
-        ((X & 1) << 0) | ((X & 1) << 1) | ((Y & 1) << 2) | ((Y & 1) << 3) \
-        | ((Z & 1) << 4) | ((Z & 1) << 5) | ((W & 1) << 6) | ((W & 1) << 7)
+#define BLEND_MASK_i4(X, Y, Z, W) ( \
+        ((X & 1) << 0) \
+		| ((X & 1) << 1) \
+		| ((Y & 1) << 2) \
+		| ((Y & 1) << 3) \
+        | ((Z & 1) << 4) \
+		| ((Z & 1) << 5) \
+		| ((W & 1) << 6) \
+		| ((W & 1) << 7) \
+		)
 
 namespace vectorization
 {
 
 #pragma region Packed types
-    //{Packed types
+	//{Packed types
 
-    typedef __m128 PackedFloat4_128;
-    typedef __m128d PackedFloat2_128;
-    typedef __m128i PackedInts_128;
+	typedef __m128 PackedFloat4_128;
+	typedef __m128d PackedFloat2_128;
+	typedef __m128i PackedInts_128;
 
-    template <typename TValueType, ASizeT Size>
-    struct PackedTypes
-    {
-        static const ASizeT SIZE = Size;
-        typedef TValueType ValueType;
-    };
+	template <typename TValueType, ASizeT Size>
+	struct PackedTypes
+	{
+		static const ASizeT SIZE = Size;
+		typedef TValueType ValueType;
+	};
 
-    template<>
-    struct PackedTypes < Float_32, 4 >
-    {
-        typedef PackedFloat4_128 Type;
-    };
+	template<>
+	struct PackedTypes < Float_32, 4 >
+	{
+		typedef PackedFloat4_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < Float_64, 2 >
-    {
-        typedef PackedFloat2_128 Type;
-    };
+	template<>
+	struct PackedTypes < Float_64, 2 >
+	{
+		typedef PackedFloat2_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_8, 16 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < Int_8, 16 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_8, 16 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_8, 16 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_16, 8 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < Int_16, 8 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_16, 8 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_16, 8 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_32, 4 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < Int_32, 4 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_32, 4 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_32, 4 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_64, 2 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < Int_64, 2 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_64, 2 >
-    {
-        typedef PackedInts_128 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_64, 2 >
+	{
+		typedef PackedInts_128 Type;
+	};
 
 
 #if VECTORIZATION_INTRINSICS_LEVEL >= VECTORIZATION_AVX
 
-    typedef __m256 PackedFloat8_256;
-    typedef __m256d PackedFloat4_256;
-    typedef __m256i PackedInts_256;
+	typedef __m256 PackedFloat8_256;
+	typedef __m256d PackedFloat4_256;
+	typedef __m256i PackedInts_256;
 
-    template<>
-    struct PackedTypes < Float_32, 8 >
-    {
-        typedef PackedFloat8_256 Type;
-    };
+	template<>
+	struct PackedTypes < Float_32, 8 >
+	{
+		typedef PackedFloat8_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < Float_64, 4 >
-    {
-        typedef PackedFloat4_256 Type;
-    };
+	template<>
+	struct PackedTypes < Float_64, 4 >
+	{
+		typedef PackedFloat4_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_8, 32 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < Int_8, 32 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_8, 32 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_8, 32 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_16, 16 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < Int_16, 16 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_16, 16 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_16, 16 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_32, 8 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < Int_32, 8 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_32, 8 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_32, 8 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < Int_64, 4 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < Int_64, 4 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
-    template<>
-    struct PackedTypes < UInt_64, 4 >
-    {
-        typedef PackedInts_256 Type;
-    };
+	template<>
+	struct PackedTypes < UInt_64, 4 >
+	{
+		typedef PackedInts_256 Type;
+	};
 
 #endif
 
-    //}
+	//}
 #pragma endregion
 
 #pragma region Alignments
-    //{Alignments
+	//{Alignments
 
-    void free(void * & data);
+	void free(void * & data) noexcept;
 
-    void * alloc(const ASizeT size);
+	void * alloc(const ASizeT size) noexcept;
 
-    void * alloc(const ASizeT size, const ASizeT alignment);
+	void * alloc(const ASizeT size, const ASizeT alignment) noexcept;
 
-    struct VectorAlignments
-    {
-        static const ASizeT ARCH = ARCH_ALIGNMENT;
-        static const ASizeT MM = MM_ALIGNMENT;
-        static const ASizeT XMM = XMM_ALIGNMENT;
-        static const ASizeT YMM = YMM_ALIGNMENT;
-        static const ASizeT ZMM = ZMM_ALIGNMENT;
-        static const ASizeT Best = BEST_ALIGNMENT;
-    };
+	void * alloc(const ASizeT size, const std::align_val_t alignment) noexcept;
 
-    template <class T>
-    struct AlignedAllocator
-    {
-        // typedefs required for xmemory
-        typedef T value_type;
-        typedef T * pointer;
-        typedef ASizeT size_type;
-        
+	struct VectorAlignments
+	{
+		static const ASizeT ARCH = ARCH_ALIGNMENT;
+		static const ASizeT MM = MM_ALIGNMENT;
+		static const ASizeT XMM = XMM_ALIGNMENT;
+		static const ASizeT YMM = YMM_ALIGNMENT;
+		static const ASizeT ZMM = ZMM_ALIGNMENT;
+		static const ASizeT Best = BEST_ALIGNMENT;
+	};
+
+	template <class T>
+	struct AlignedAllocator
+	{
+		// typedefs required for xmemory
+		typedef T value_type;
+		typedef T * pointer;
+		typedef ASizeT size_type;
+
 		AlignedAllocator() noexcept
 		{
 		}
-        
-		template<class U> AlignedAllocator(const AlignedAllocator<U> &) noexcept
+
+		template<class U>
+		AlignedAllocator(const AlignedAllocator<U> &) noexcept
 		{
 		}
-        
+
 		template<class U>
 		const bool operator==(const AlignedAllocator<U> &) const
 		{
 			return true;
 		}
-        
+
 		template<class U>
 		const bool operator!=(const AlignedAllocator<U> &) const
 		{
 			return false;
 		}
 
-        pointer const allocate(const size_type n) const
-        {
+		pointer const allocate(const size_type n) const
+		{
 			if (n == 0) {
 				return nullptr;
 			}
-            if (n > static_cast<size_type>(-1) / sizeof(T)) {
-                throw std::bad_array_new_length();
-            }
+			if (n > static_cast<size_type>(-1) / sizeof(T)) {
+				throw std::bad_array_new_length();
+			}
 
-            void* const pv = alloc(sizeof(T) * n, __alignof(T));
+			void* const pv = alloc(sizeof(T) * n, __alignof(T));
 
-            if (!pv) {
+			if (!pv) {
 				throw std::bad_alloc();
 			}
 
 			return static_cast<pointer const>(pv);
-        }
+		}
 
-        void deallocate(pointer const p, const size_type) const noexcept
-        {
-            if (p) {
-                void * vp = reinterpret_cast<void *>(p);
-                free(vp);
-            }
-        }
+		void deallocate(pointer const p, const size_type) const
+		{
+			if (p) {
+				void * vp = reinterpret_cast<void *>(p);
+				free(vp);
+			}
+		}
 
-        // templated alloc
-        static pointer const allocElements(const size_type n, const size_type alignment)
-        {
-            return static_cast<pointer const>(alloc(sizeof(T) * n, alignment));
-        }
-        static pointer const allocElements(const size_type n)
-        {
-            return allocElements(n, __alignof(T));
-        }
-    };
+		// templated alloc
+		static pointer const allocElements(const size_type n, const size_type alignment)
+		{
+			return static_cast<pointer const>(alloc(sizeof(T) * n, alignment));
+		}
+		static pointer const allocElements(const size_type n)
+		{
+			return allocElements(n, __alignof(T));
+		}
+	};
 
-    //}
+	//}
 #pragma endregion
 
 }
--- trunk/vectorization/src/vectorization/intrinsics.cpp
+++ trunk/vectorization/src/vectorization/intrinsics.cpp
@@ -10,23 +10,28 @@ namespace vectorization
     const ASizeT VectorAlignments::ZMM;
     const ASizeT VectorAlignments::Best;
 
-    void free(void * & data)
+    void free(void * & data) noexcept
     {
         assert(data);
         _mm_free(data);
         data = nullptr;
     }
 
-    void * alloc(const ASizeT size)
+    void * alloc(const ASizeT size) noexcept
     {
         return alloc(size, VectorAlignments::Best);
     }
 
-    void * alloc(const ASizeT size, const ASizeT alignment)
+    void * alloc(const ASizeT size, const ASizeT alignment) noexcept
     {
         void * data = _mm_malloc(size, alignment);
         assert(data);
         return data;
     }
+
+	void * alloc(const ASizeT size, const std::align_val_t alignment) noexcept
+	{
+		return alloc(size, ASizeT(alignment));
+	}
 
 }
