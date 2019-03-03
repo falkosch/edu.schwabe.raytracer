a9282e8b86c73d9cf1fec545aac1e6a6057cf46f
Falko Schwabe <falkosch@hotmail.de>
2019-03-03 23:17:16 +0100

    provide tests for == and != operators for v_f32_4 type

--- trunk/vectorization.native-test/src/vectorization/v_f32_4/functions_test.cpp
+++ trunk/vectorization.native-test/src/vectorization/v_f32_4/functions_test.cpp
@@ -10,32 +10,9 @@ namespace vectorization
 	namespace test
 	{
 
-		TEST_CLASS(functions_testTest)
+		TEST_CLASS(v_f32_4_FunctionsTest)
 		{
 		public:
-
-			TEST_METHOD(testExample1)
-			{
-				auto expected = 0;
-				auto actual = 0;
-				Assert::AreEqual(expected, actual, L"", LINE_INFO());
-			}
-
-			TEST_METHOD(testExample2)
-			{
-				auto expected = 0;
-				auto actual = 1;
-				Assert::AreEqual(expected, actual, L"", LINE_INFO());
-			}
-
-			TEST_METHOD(testExample3)
-			{
-				std::array<vectorization::UInt_32, 5> specimen = { 0, 1, 2, 3, 4 };
-
-				for (const auto & v : specimen) {
-					Assert::IsTrue(v > -1, L"", LINE_INFO());
-				}
-			}
 
 		};
 
--- trunk/vectorization.native-test/src/vectorization/v_f32_4/operators_test.cpp
+++ trunk/vectorization.native-test/src/vectorization/v_f32_4/operators_test.cpp
@@ -10,34 +10,88 @@ namespace vectorization
 	namespace test
 	{
 
-		TEST_CLASS(operators_testTest)
+		TEST_CLASS(v_f32_4_OperatorsTest)
 		{
 		public:
 
-			TEST_METHOD(testExample1)
-			{
-				auto expected = 0;
-				auto actual = 0;
-				Assert::AreEqual(expected, actual, L"", LINE_INFO());
+			static std::array<v_f32_4::ValueType, v_f32_4::SIZE> sample1;
+			static std::array<v_f32_4::ValueType, v_f32_4::SIZE> sample2;
+
+			static v_f32_4 sampleVector1;
+			static v_f32_4 sampleVector2;
+
+			TEST_CLASS_INITIALIZE(v_f32_4_OperatorsTestInitialize) {
+				sample1 = {
+					Zero<v_f32_4::ValueType>(),
+					One<v_f32_4::ValueType>(),
+					Infinity<v_f32_4::ValueType>(),
+					NotANumber<v_f32_4::ValueType>(),
+				};
+
+				sample2 = {
+					NegativeZero<v_f32_4::ValueType>(),
+					NegativeOne<v_f32_4::ValueType>(),
+					NegativeInfinity<v_f32_4::ValueType>(),
+					-NotANumber<v_f32_4::ValueType>(),
+				};
+
+				sampleVector1 = v_f32_4(
+					sample1.at(VectorIndices::X),
+					sample1.at(VectorIndices::Y),
+					sample1.at(VectorIndices::Z),
+					sample1.at(VectorIndices::W)
+				);
+
+				sampleVector2 = v_f32_4(
+					sample2.at(VectorIndices::X),
+					sample2.at(VectorIndices::Y),
+					sample2.at(VectorIndices::Z),
+					sample2.at(VectorIndices::W)
+				);
 			}
 
-			TEST_METHOD(testExample2)
+			TEST_METHOD(testEqualsOperator)
 			{
-				auto expected = 0;
-				auto actual = 1;
-				Assert::AreEqual(expected, actual, L"", LINE_INFO());
+				auto actual = sampleVector1 == sampleVector2;
+				auto expected = v_f32_4::VectorBoolType(MaskAll<v_f32_4::BoolType>(), 0, 0, 0);
+				Assert::AreEqual(x(expected), x(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(y(expected), y(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(z(expected), z(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(w(expected), w(actual), L"Equals value mismatch", LINE_INFO());
+
+				auto sampleVector3 = v_f32_4(sampleVector1);
+				actual = sampleVector1 == sampleVector3;
+				expected = v_f32_4::VectorBoolType(MaskAll<v_f32_4::BoolType>(), MaskAll<v_f32_4::BoolType>(), MaskAll<v_f32_4::BoolType>(), 0);
+				Assert::AreEqual(x(expected), x(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(y(expected), y(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(z(expected), z(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(w(expected), w(actual), L"Equals value mismatch", LINE_INFO());
 			}
 
-			TEST_METHOD(testExample3)
+			TEST_METHOD(testNotEqualsOperator)
 			{
-				std::array<vectorization::UInt_32, 5> specimen = { 0, 1, 2, 3, 4 };
+				auto actual = sampleVector1 != sampleVector2;
+				auto expected = v_f32_4::VectorBoolType(0, MaskAll<v_f32_4::BoolType>(), MaskAll<v_f32_4::BoolType>(), MaskAll<v_f32_4::BoolType>());
+				Assert::AreEqual(x(expected), x(actual), L"NotEquals value mismatch", LINE_INFO());
+				Assert::AreEqual(y(expected), y(actual), L"NotEquals value mismatch", LINE_INFO());
+				Assert::AreEqual(z(expected), z(actual), L"NotEquals value mismatch", LINE_INFO());
+				Assert::AreEqual(w(expected), w(actual), L"NotEquals value mismatch", LINE_INFO());
 
-				for (const auto & v : specimen) {
-					Assert::IsTrue(v > -1, L"", LINE_INFO());
-				}
+				auto sampleVector3 = v_f32_4(sampleVector1);
+				actual = sampleVector1 != sampleVector3;
+				expected = v_f32_4::VectorBoolType(0, 0, 0, MaskAll<v_f32_4::BoolType>());
+				Assert::AreEqual(x(expected), x(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(y(expected), y(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(z(expected), z(actual), L"Equals value mismatch", LINE_INFO());
+				Assert::AreEqual(w(expected), w(actual), L"Equals value mismatch", LINE_INFO());
 			}
 
 		};
+
+		std::array<v_f32_4::ValueType, 4> v_f32_4_OperatorsTest::sample1;
+		std::array<v_f32_4::ValueType, 4> v_f32_4_OperatorsTest::sample2;
+		v_f32_4 v_f32_4_OperatorsTest::sampleVector1;
+		v_f32_4 v_f32_4_OperatorsTest::sampleVector2;
 
 	}
 }
