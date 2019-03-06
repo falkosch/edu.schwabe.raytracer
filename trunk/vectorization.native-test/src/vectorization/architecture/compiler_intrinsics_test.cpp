205c0d148f38d30848d8b90fba07cfb2cae3fe92
Falko Schwabe <falkosch@hotmail.de>
2019-03-06 01:08:04 +0100

    clean up test code

--- trunk/vectorization.native-test/src/vectorization/architecture/compiler_intrinsics_test.cpp
+++ trunk/vectorization.native-test/src/vectorization/architecture/compiler_intrinsics_test.cpp
@@ -16,32 +16,64 @@ namespace vectorization
 
 			TEST_METHOD(testGeneratesMasksForFloat4_32)
 			{
-				Assert::AreEqual(0 + 0 + 0 + 0,
-					BLEND_MASK_FLOAT4_32(0, 0, 0, 0), L" 0 != MASK(0,0,0,0)", LINE_INFO());
+				Assert::AreEqual(
+					0 + 0 + 0 + 0,
+					BLEND_MASK_FLOAT4_32(0, 0, 0, 0), 
+					L" 0 != MASK(0,0,0,0)", 
+					LINE_INFO()
+				);
 
-				Assert::AreEqual(1 + 0 + 4 + 0,
-					BLEND_MASK_FLOAT4_32(1, 0, 1, 0), L" 5 != MASK(1,0,1,0)", LINE_INFO());
+				Assert::AreEqual(
+					1 + 0 + 4 + 0,
+					BLEND_MASK_FLOAT4_32(1, 0, 1, 0),
+					L" 5 != MASK(1,0,1,0)", 
+					LINE_INFO()
+				);
 
-				Assert::AreEqual(0 + 2 + 0 + 8,
-					BLEND_MASK_FLOAT4_32(0, 1, 0, 1), L"10 != MASK(0,1,0,1)", LINE_INFO());
+				Assert::AreEqual(
+					0 + 2 + 0 + 8,
+					BLEND_MASK_FLOAT4_32(0, 1, 0, 1), 
+					L"10 != MASK(0,1,0,1)",
+					LINE_INFO()
+				);
 
-				Assert::AreEqual(1 + 2 + 4 + 8,
-					BLEND_MASK_FLOAT4_32(1, 1, 1, 1), L"15 != MASK(1,1,1,1)", LINE_INFO());
+				Assert::AreEqual(
+					1 + 2 + 4 + 8,
+					BLEND_MASK_FLOAT4_32(1, 1, 1, 1), 
+					L"15 != MASK(1,1,1,1)", 
+					LINE_INFO()
+				);
 			}
 
 			TEST_METHOD(testGeneratesMasksForInt4_32)
 			{
-				Assert::AreEqual(0 + 0 + 0 + 0 + 0 + 0 + 0 + 0,
-					BLEND_MASK_INT4_32(0, 0, 0, 0), L"  0 != MASK(0,0,0,0)", LINE_INFO());
+				Assert::AreEqual(
+					0 + 0 + 0 + 0 + 0 + 0 + 0 + 0,
+					BLEND_MASK_INT4_32(0, 0, 0, 0), 
+					L"  0 != MASK(0,0,0,0)", 
+					LINE_INFO()
+				);
 
-				Assert::AreEqual(1 + 2 + 0 + 0 + 16 + 32 + 0 + 0,
-					BLEND_MASK_INT4_32(1, 0, 1, 0), L" 51 != MASK(1,0,1,0)", LINE_INFO());
+				Assert::AreEqual(
+					1 + 2 + 0 + 0 + 16 + 32 + 0 + 0,
+					BLEND_MASK_INT4_32(1, 0, 1, 0), 
+					L" 51 != MASK(1,0,1,0)", 
+					LINE_INFO()
+				);
 
-				Assert::AreEqual(0 + 0 + 4 + 8 + 0 + 0 + 64 + 128,
-					BLEND_MASK_INT4_32(0, 1, 0, 1), L"204 != MASK(0,1,0,1)", LINE_INFO());
+				Assert::AreEqual(
+					0 + 0 + 4 + 8 + 0 + 0 + 64 + 128,
+					BLEND_MASK_INT4_32(0, 1, 0, 1), 
+					L"204 != MASK(0,1,0,1)",
+					LINE_INFO()
+				);
 
-				Assert::AreEqual(1 + 2 + 4 + 8 + 16 + 32 + 64 + 128,
-					BLEND_MASK_INT4_32(1, 1, 1, 1), L"255 != MASK(1,1,1,1)", LINE_INFO());
+				Assert::AreEqual(
+					1 + 2 + 4 + 8 + 16 + 32 + 64 + 128,
+					BLEND_MASK_INT4_32(1, 1, 1, 1), 
+					L"255 != MASK(1,1,1,1)", 
+					LINE_INFO()
+				);
 			}
 
 			TEST_METHOD(testConvertsUnsignedIntegersToFloats)
