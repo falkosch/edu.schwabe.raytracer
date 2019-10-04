e5c811db2ea32394a5eb34e6a81558a3663aaab4
Falko Schwabe <falkosch@hotmail.de>
2019-10-04 21:12:37 +0200

    implement blend tests

--- trunk/vectorization.native-test/src/vectorization/v_f32_4/blends_test.cpp
+++ trunk/vectorization.native-test/src/vectorization/v_f32_4/blends_test.cpp
@@ -16,54 +16,195 @@ namespace vectorization
         {
         public:
 
-            static const std::array<v_f32_4::ValueType, v_f32_4::SIZE> sampleArray() {
-                return StandardSample::ofArrayType<v_f32_4::ValueType, v_f32_4::SIZE>();
-            }
-
-            static const v_f32_4 sampleVector() {
-                return StandardSample::ofVectorType<v_f32_4>();
-            }
-
-            TEST_METHOD(blendsXYZWGenerically)
+            TEST_METHOD(genericallyBlendsIdentities)
             {
-                auto givenVector = One<v_f32_4>();
-                auto expectedBlends = Two<v_f32_4>();
+                auto given = One<v_f32_4>();
+                auto expectedXYZW = Two<v_f32_4>();
 
                 {
-                    auto testBlended = blend<false, false, false, false>(givenVector, expectedBlends);
-                    Assert::IsTrue(allTrue(expectedBlends != testBlended));
+                    auto actual = blend<false, false, false, false>(given, expectedXYZW);
+                    Assert::IsTrue(allTrue(expectedXYZW != actual), L"blend mismatch", LINE_INFO());
                 }
 
                 {
-                    auto testBlended = blend<true, false, false, false>(givenVector, expectedBlends);
-                    Assert::AreEqual(x(expectedBlends), x(testBlended));
-                    Assert::AreNotEqual(y(expectedBlends), y(testBlended));
-                    Assert::AreNotEqual(z(expectedBlends), z(testBlended));
-                    Assert::AreNotEqual(w(expectedBlends), w(testBlended));
+                    auto actualXYZW = blend<true, true, true, true>(given, expectedXYZW);
+                    Assert::IsTrue(allTrue(expectedXYZW == actualXYZW), L"blend mismatch", LINE_INFO());
+                }
+            }
+
+            TEST_METHOD(genericallyBlendsIndividuals)
+            {
+                auto given = One<v_f32_4>();
+                auto expectedXYZW = Two<v_f32_4>();
+
+                {
+                    auto actualX = blend<true, false, false, false>(given, expectedXYZW);
+                    Assert::AreEqual(x(expectedXYZW), x(actualX), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualX), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualX), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualX), L"blend mismatch", LINE_INFO());
                 }
 
                 {
-                    auto testBlended = blend<false, true, false, false>(givenVector, expectedBlends);
-                    Assert::AreNotEqual(x(expectedBlends), x(testBlended));
-                    Assert::AreEqual(y(expectedBlends), y(testBlended));
-                    Assert::AreNotEqual(z(expectedBlends), z(testBlended));
-                    Assert::AreNotEqual(w(expectedBlends), w(testBlended));
+                    auto actualY = blend<false, true, false, false>(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualY), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualY), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualY), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualY), L"blend mismatch", LINE_INFO());
                 }
 
                 {
-                    auto testBlended = blend<false, false, true, false>(givenVector, expectedBlends);
-                    Assert::AreNotEqual(x(expectedBlends), x(testBlended));
-                    Assert::AreNotEqual(y(expectedBlends), y(testBlended));
-                    Assert::AreEqual(z(expectedBlends), z(testBlended));
-                    Assert::AreNotEqual(w(expectedBlends), w(testBlended));
+                    auto actualZ = blend<false, false, true, false>(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualZ), L"blend mismatch", LINE_INFO());
                 }
 
                 {
-                    auto testBlended = blend<false, false, false, true>(givenVector, expectedBlends);
-                    Assert::AreNotEqual(x(expectedBlends), x(testBlended));
-                    Assert::AreNotEqual(y(expectedBlends), y(testBlended));
-                    Assert::AreNotEqual(z(expectedBlends), z(testBlended));
-                    Assert::AreEqual(w(expectedBlends), w(testBlended));
+                    auto actualW = blend<false, false, false, true>(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualW), L"blend mismatch", LINE_INFO());
+                }
+            }
+
+            TEST_METHOD(genericallyBlendsPairs)
+            {
+                auto given = One<v_f32_4>();
+                auto expectedXYZW = Two<v_f32_4>();
+
+                {
+                    auto actualXY = blend<true, true, false, false>(given, expectedXYZW);
+                    Assert::AreEqual(x(expectedXYZW), x(actualXY), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualXY), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualXY), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualXY), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualXZ = blend<true, false, true, false>(given, expectedXYZW);
+                    Assert::AreEqual(x(expectedXYZW), x(actualXZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualXZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualXZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualXZ), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualYW = blend<false, true, false, true>(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualYW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualYW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualYW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualYW), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualZW = blend<false, false, true, true>(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualZW), L"blend mismatch", LINE_INFO());
+                }
+            }
+
+            TEST_METHOD(genericallyBlendsTriplets)
+            {
+                auto given = One<v_f32_4>();
+                auto expectedXYZW = Two<v_f32_4>();
+
+                {
+                    auto actualYZW = blend<false, true, true, true>(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualYZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualYZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualYZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualYZW), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualXZW = blend<true, false, true, true>(given, expectedXYZW);
+                    Assert::AreEqual(x(expectedXYZW), x(actualXZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualXZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualXZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualXZW), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualXYW = blend<true, true, false, true>(given, expectedXYZW);
+                    Assert::AreEqual(x(expectedXYZW), x(actualXYW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualXYW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualXYW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualXYW), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualXYZ = blend<true, true, true, false>(given, expectedXYZW);
+                    Assert::AreEqual(x(expectedXYZW), x(actualXYZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualXYZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualXYZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualXYZ), L"blend mismatch", LINE_INFO());
+                }
+            }
+
+            TEST_METHOD(hasShortFormForBlends)
+            {
+                auto given = One<v_f32_4>();
+                auto expectedXYZW = Two<v_f32_4>();
+
+                {
+                    auto actualYZW = x_yzw(given, expectedXYZW);
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualYZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualYZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualYZW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualYZW), L"blend mismatch", LINE_INFO());
+                }
+            }
+
+            TEST_METHOD(dynamicallyBlendsWithMaskOfSameType)
+            {
+                auto given = One<v_f32_4>();
+                auto expectedXYZW = Two<v_f32_4>();
+
+                {
+                    auto actual = blendMasked(given, expectedXYZW, MaskNone<v_f32_4>());
+                    Assert::IsTrue(allTrue(expectedXYZW != actual), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualXYZW = blendMasked(given, expectedXYZW, MaskAll<v_f32_4>());
+                    Assert::IsTrue(allTrue(expectedXYZW == actualXYZW), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualX = blendMasked(given, expectedXYZW, MaskX<v_f32_4>());
+                    Assert::AreEqual(x(expectedXYZW), x(actualX), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualX), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualX), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualX), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualY = blendMasked(given, expectedXYZW, MaskY<v_f32_4>());
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualY), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(y(expectedXYZW), y(actualY), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualY), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualY), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualZ = blendMasked(given, expectedXYZW, MaskZ<v_f32_4>());
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(z(expectedXYZW), z(actualZ), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(w(expectedXYZW), w(actualZ), L"blend mismatch", LINE_INFO());
+                }
+
+                {
+                    auto actualW = blendMasked(given, expectedXYZW, MaskW<v_f32_4>());
+                    Assert::AreNotEqual(x(expectedXYZW), x(actualW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(y(expectedXYZW), y(actualW), L"blend mismatch", LINE_INFO());
+                    Assert::AreNotEqual(z(expectedXYZW), z(actualW), L"blend mismatch", LINE_INFO());
+                    Assert::AreEqual(w(expectedXYZW), w(actualW), L"blend mismatch", LINE_INFO());
                 }
             }
 
