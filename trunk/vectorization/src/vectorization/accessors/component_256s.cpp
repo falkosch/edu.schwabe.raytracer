425413e0b5bcc7238b7606ca539fe4f81de81df7
Falko Schwabe <falkosch@hotmail.de>
2019-10-11 01:35:14 +0200

    implement component accessors for PackedFloat_256

--- trunk/vectorization/src/vectorization/accessors/component_256s.cpp
+++ trunk/vectorization/src/vectorization/accessors/component_256s.cpp
@@ -2,6 +2,18 @@
 
 namespace vectorization
 {
+    inline const PackedFloat8_256 hi128_lo128(const PackedFloat8_256 v) {
+        return _mm256_permute2f128_ps(v, v, 0b00000001);
+    }
+
+    inline const PackedFloat8_256 zxzx_zxzx(const PackedFloat8_256 v) {
+        return _mm256_permute_ps(v, 0b00100010);
+    }
+
+    inline const PackedFloat8_256 wywy_wywy(const PackedFloat8_256 v) {
+        return _mm256_permute_ps(v, 0b01110111);
+    }
+
     template <>
     const Float_32 component<VectorIndices::X1>(const PackedFloat8_256 & v) noexcept {
         return _mm256_cvtss_f32(v);
@@ -9,44 +21,37 @@ namespace vectorization
 
     template <>
     const Float_32 component<VectorIndices::X2>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(_mm256_movehdup_ps(v));
     }
 
     template <>
     const Float_32 component<VectorIndices::X3>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(zxzx_zxzx(v));
     }
 
     template <>
     const Float_32 component<VectorIndices::X4>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(wywy_wywy(v));
     }
 
     template <>
     const Float_32 component<VectorIndices::X5>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(hi128_lo128(v));
     }
 
     template <>
     const Float_32 component<VectorIndices::X6>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(_mm256_movehdup_ps(hi128_lo128(v)));
     }
 
     template <>
     const Float_32 component<VectorIndices::X7>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(zxzx_zxzx(hi128_lo128(v)));
     }
 
     template <>
     const Float_32 component<VectorIndices::X8>(const PackedFloat8_256 & v) noexcept {
-        // TODO
-        return _mm256_cvtss_f32(v);
+        return _mm256_cvtss_f32(wywy_wywy(hi128_lo128(v)));
     }
 
     const Float_32 x(const PackedFloat8_256 & v) noexcept {
