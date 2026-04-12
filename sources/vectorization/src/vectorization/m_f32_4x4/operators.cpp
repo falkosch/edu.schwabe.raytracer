84a9813f46b730a4b2fd5cb161b49a97038643ff
Falko Schwabe <2913295+falkosch@users.noreply.github.com>
2026-04-12 19:41:58 +0200

    refactor: optimize matrix-vector multiplication using `hadd` for improved efficiency and readability

--- sources/vectorization/src/vectorization/m_f32_4x4/operators.cpp
+++ sources/vectorization/src/vectorization/m_f32_4x4/operators.cpp
@@ -21,26 +21,18 @@ namespace vectorization {
   }
 
   m_f32_4x4::RowVectorType operator*(const m_f32_4x4 &matrix, const m_f32_4x4::RowVectorType &vector) noexcept {
-    // #if VECTORIZATION_INTRINSICS_LEVEL >= VECTORIZATION_AVX
-    //         const auto vv = _mm256_broadcast_ps(&v.components);
-    //         const auto ha_02_13 = _mm256_hadd_ps(
-    //             _mm256_mul_ps(vv, _mm256_load_ps(reinterpret_cast<const m_f32_4x4::ValueType * const>(&m.row0))),
-    //             _mm256_mul_ps(vv, _mm256_load_ps(reinterpret_cast<const m_f32_4x4::ValueType * const>(&m.row2))));
-    //         //_mm256_mul_ps(vv, _mm256_set_m128(m.row1.components, m.row0.components)),
-    //         //_mm256_mul_ps(vv, _mm256_set_m128(m.row3.components, m.row2.components)));
-    //         const auto r_0213_1302 = _mm256_hadd_ps(
-    //             ha_02_13,
-    //             _mm256_permute2f128_ps(ha_02_13, ha_02_13, 0x01));
-    //         return _mm256_castps256_ps128(_mm256_permute_ps(
-    //             r_0213_1302,
-    //             _MM_SHUFFLE(VectorIndices::W, VectorIndices::Y, VectorIndices::Z, VectorIndices::X)));
-    // #else
-    const auto d0 = _mm_dp_ps(vector.components, matrix.row0.components, 0xF1);
-    const auto d1 = _mm_dp_ps(vector.components, matrix.row1.components, 0xF2);
-    const auto d2 = _mm_dp_ps(vector.components, matrix.row2.components, 0xF4);
-    const auto d3 = _mm_dp_ps(vector.components, matrix.row3.components, 0xF8);
-    return _mm_or_ps(_mm_or_ps(d0, d1), _mm_or_ps(d2, d3));
-    // #endif
+    // dpps variant: 4x dpps (9-13cy) + 3x orps (1cy) = ~11-15cy chain (dpps parallel, then or-tree)
+    // const auto d0 = _mm_dp_ps(vector.components, matrix.row0.components, 0xF1);
+    // const auto d1 = _mm_dp_ps(vector.components, matrix.row1.components, 0xF2);
+    // const auto d2 = _mm_dp_ps(vector.components, matrix.row2.components, 0xF4);
+    // const auto d3 = _mm_dp_ps(vector.components, matrix.row3.components, 0xF8);
+    // return _mm_or_ps(_mm_or_ps(d0, d1), _mm_or_ps(d2, d3));
+
+    // hadd variant: 4x mulps (5cy) + 3x haddps (3-6cy) = ~11-17cy chain (mul parallel, then hadd-tree)
+    return _mm_hadd_ps(
+        _mm_hadd_ps((vector * matrix.row0).components, (vector * matrix.row1).components),
+        _mm_hadd_ps((vector * matrix.row2).components, (vector * matrix.row3).components)
+    );
   }
 
   m_f32_4x4::ColumnVectorType operator*(const m_f32_4x4::ColumnVectorType &vector, const m_f32_4x4 &matrix) noexcept {
